6cf80a9fe4da275b1a7b1bbfbc75344637603f1c
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-13 19:51:06 +0100

    variables
    p131 definive guide to javascript

---
+++ variables.js
@@ -0,0 +1,9 @@
+//Anything followgin double slashes is an english-language comment.
+// Read the comments carefully: they explain the javascript code.
+
+// variable is a symbolic name for a value.
+// Varuiables are declared with the war keyboard.
+
+var x;	/Declare a variable named x.
+
+//Value 
